bbfad71573c323b64b25724a2938f7ee2c4b607c
vimalestrrado <vimalvyshagam@gmail.com>
2022-01-21 22:36:05 +0530

    al

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./pages/Home";
-import Tables from "./pages/Tables";
-import Billing from "./pages/Billing";
-import Rtl from "./pages/Rtl";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import NewOrder from "./pages/NewOrder";
-import TestPage from "./pages/TestPage";
+// import { Switch, Route, Redirect } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Tables from "./pages/Tables";
+// import Billing from "./pages/Billing";
+// import Rtl from "./pages/Rtl";
+// import Profile from "./pages/Profile";
+// import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import NewOrder from "./pages/NewOrder";
+// import TestPage from "./pages/TestPage";
 
-import Main from "./components/layout/Main";
-import "antd/dist/antd.css";
-import "./assets/styles/main.css";
-import "./assets/styles/responsive.css";
+// import Main from "./components/layout/Main";
+// import "antd/dist/antd.css";
+// import "./assets/styles/main.css";
+// import "./assets/styles/responsive.css";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" exact component={SignIn} />
+      hello
       {/* <Switch>
         <Redirect exact from="/" to="/sign-in" />
         <Route path="/sign-up" exact component={SignUp} />
